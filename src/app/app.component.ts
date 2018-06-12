c2a07a25ee052b2b40b7c1759148222b54455a4a
Magali <magali_thuaire@yahoo.fr>
2018-06-12 16:42:17 +0200

    ajout firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
 	selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
 	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
+	constructor() {
+		// Initialize Firebase
+		var config = {
+			apiKey: "AIzaSyB8iuK9yLlvXraMMpv1KwNMq4Rf_tH_sCE",
+			authDomain: "blog-911fd.firebaseapp.com",
+			databaseURL: "https://blog-911fd.firebaseio.com",
+			projectId: "blog-911fd",
+			storageBucket: "",
+			messagingSenderId: "279989502477"
+		};
+		firebase.initializeApp(config);
+	}
 }
