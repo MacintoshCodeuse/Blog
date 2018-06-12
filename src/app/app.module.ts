9125422bd0a2011004b4b1ccb998e7f6e94830fc
Magali <magali_thuaire@yahoo.fr>
2018-06-12 12:01:41 +0200

    ajout d'un header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,20 +6,22 @@ import { AppComponent } from './app.component';
 import { PostListComponent } from './post-list/post-list.component';
 import { postsService } from './services/postsService';
 import { PostListItemComponent } from './post-list/post-list-item/post-list-item.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    PostListComponent,
-    PostListItemComponent
-  ],
-  imports: [
-	BrowserModule,
-	AngularFontAwesomeModule
-  ],
-  providers: [
-	  postsService
-  ],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		PostListComponent,
+		PostListItemComponent,
+		HeaderComponent
+	],
+	imports: [
+		BrowserModule,
+		AngularFontAwesomeModule
+	],
+	providers: [
+		postsService
+	],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
